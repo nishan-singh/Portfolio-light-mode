edcddf0e1f2356b80399f7cea1b97f00ef463961
Nishan Singh <singh007nishan@gmail.com>
2023-06-20 17:41:00 +0200

    completed header and introduction

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @ViewChild('navLinks') navLinks: any;
 
+  toggleNavLinks() {
+    this.navLinks.nativeElement.classList.toggle('show-nav-links');
+  }
 }
