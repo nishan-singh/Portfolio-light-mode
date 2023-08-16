46d51c7080ba4762e847452c257bb33893125a37
Nishan Singh <singh007nishan@gmail.com>
2023-08-16 21:27:06 +0200

    added highlight animations

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { annotate } from 'rough-notation';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent {
+  @ViewChild('highlightElm') hightLightElm: any;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    let highLight = document.querySelector('#highlightElm') as HTMLElement;
+    let annotation = annotate(highLight, {
+      type: 'highlight',
+      color: '#f9d423',
+      padding: [5, 2],
+      multiline: true,
+    });
+    annotation.show();
+  }
 }
