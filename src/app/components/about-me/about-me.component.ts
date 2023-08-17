502afacb1d9853cacfdca5301e81f3234a7f3843
Nishan Singh <singh007nishan@gmail.com>
2023-08-17 10:41:57 +0200

    added highlighting

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -15,21 +15,58 @@ export class AboutMeComponent {
   }
 
   markText() {
-    let markSetting = {
-      type: 'highlight',
-      color: '#f9d423',
-      multiline: true,
-      padding: [2, 2],
-    } as RoughAnnotationConfig;
     let markElem1 = annotate(
       document.querySelector('#highlight1') as HTMLElement,
-      markSetting
+      this.markSetting('#A8DF8E')
     );
     let markElem2 = annotate(
       document.querySelector('#highlight2') as HTMLElement,
-      markSetting
+      this.markSetting('#f9d423')
     );
-    this.annotation = annotationGroup([markElem1, markElem2]);
-    // this.annotation.show();
+    let markElem3 = annotate(
+      document.querySelector('#highlight3') as HTMLElement,
+      this.markSetting('#BEADFA')
+    );
+    let markElem4 = annotate(
+      document.querySelector('#highlight4') as HTMLElement,
+      this.markSetting('#8BE8E5')
+    );
+    let markElem5 = annotate(
+      document.querySelector('#highlight5') as HTMLElement,
+      this.markSetting('#FEA1A1')
+    );
+    let markElem6 = annotate(
+      document.querySelector('#highlight6') as HTMLElement,
+      this.markSetting('#FE7BE5')
+    );
+    let markElem7 = annotate(
+      document.querySelector('#highlight7') as HTMLElement,
+      this.markSetting('#b1dedc')
+    );
+    let markElem8 = annotate(
+      document.querySelector('#highlight8') as HTMLElement,
+      this.markSetting('#A1C2F1')
+    );
+
+    this.annotation = annotationGroup([
+      markElem1,
+      markElem2,
+      markElem3,
+      markElem4,
+      markElem5,
+      markElem6,
+      markElem7,
+      markElem8,
+    ]);
+    this.annotation.show();
+  }
+
+  markSetting(color: string) {
+    return {
+      type: 'highlight',
+      color: color,
+      multiline: true,
+      padding: [2, 2],
+    } as RoughAnnotationConfig;
   }
 }
