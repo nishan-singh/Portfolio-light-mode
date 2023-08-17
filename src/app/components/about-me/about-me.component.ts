5f4578af5ace5bf3204aaad9ac5337a2443ccd82
Nishan Singh <singh007nishan@gmail.com>
2023-08-17 11:33:07 +0200

    added marking and underlining

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -8,47 +8,25 @@ import { RoughAnnotationConfig } from 'rough-notation/lib/model';
   styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent {
-  annotation: any;
+  textAnnotation: any;
+  headingAnnotation: any;
 
   ngOnInit(): void {
     this.markText();
+    this.underlineHeading();
   }
 
   markText() {
-    let markElem1 = annotate(
-      document.querySelector('#highlight1') as HTMLElement,
-      this.markSetting('#A8DF8E')
-    );
-    let markElem2 = annotate(
-      document.querySelector('#highlight2') as HTMLElement,
-      this.markSetting('#f9d423')
-    );
-    let markElem3 = annotate(
-      document.querySelector('#highlight3') as HTMLElement,
-      this.markSetting('#BEADFA')
-    );
-    let markElem4 = annotate(
-      document.querySelector('#highlight4') as HTMLElement,
-      this.markSetting('#8BE8E5')
-    );
-    let markElem5 = annotate(
-      document.querySelector('#highlight5') as HTMLElement,
-      this.markSetting('#FEA1A1')
-    );
-    let markElem6 = annotate(
-      document.querySelector('#highlight6') as HTMLElement,
-      this.markSetting('#FE7BE5')
-    );
-    let markElem7 = annotate(
-      document.querySelector('#highlight7') as HTMLElement,
-      this.markSetting('#b1dedc')
-    );
-    let markElem8 = annotate(
-      document.querySelector('#highlight8') as HTMLElement,
-      this.markSetting('#A1C2F1')
-    );
+    let markElem1 = this.markElms('#highlight1', '#A8DF8E');
+    let markElem2 = this.markElms('#highlight2', '#f9d423');
+    let markElem3 = this.markElms('#highlight3', '#BEADFA');
+    let markElem4 = this.markElms('#highlight4', '#8BE8E5');
+    let markElem5 = this.markElms('#highlight5', '#FEA1A1');
+    let markElem6 = this.markElms('#highlight6', '#FE7BE5');
+    let markElem7 = this.markElms('#highlight7', '#b1dedc');
+    let markElem8 = this.markElms('#highlight8', '#A1C2F1');
 
-    this.annotation = annotationGroup([
+    this.textAnnotation = annotationGroup([
       markElem1,
       markElem2,
       markElem3,
@@ -58,7 +36,15 @@ export class AboutMeComponent {
       markElem7,
       markElem8,
     ]);
-    this.annotation.show();
+    this.textAnnotation.show();
+  }
+
+  underlineHeading() {
+    let underlineHeading = annotate(
+      document.querySelector('#hd-underline') as HTMLElement,
+      this.underlineSetting('#75C2F6')
+    );
+    underlineHeading.show();
   }
 
   markSetting(color: string) {
@@ -66,6 +52,22 @@ export class AboutMeComponent {
       type: 'highlight',
       color: color,
       multiline: true,
+      animate: true,
+    } as RoughAnnotationConfig;
+  }
+
+  markElms(id: string, color: string) {
+    return annotate(
+      document.querySelector(id) as HTMLElement,
+      this.markSetting(color)
+    );
+  }
+
+  underlineSetting(color: string) {
+    return {
+      type: 'underline',
+      color: color,
+      strokeWidth: 3,
       padding: [2, 2],
     } as RoughAnnotationConfig;
   }
