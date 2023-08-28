a6329203338f8ba56b21f20db42a706ce39eeaa7
Nishan Singh <singh007nishan@gmail.com>
2023-08-28 12:43:28 +0200

    optimized hero animation

--- src/app/components/introduction/introduction.component.ts
+++ src/app/components/introduction/introduction.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/service/data.service';
 
 @Component({
   selector: 'app-introduction',
   templateUrl: './introduction.component.html',
   styleUrls: ['./introduction.component.scss'],
 })
-export class IntroductionComponent {}
+export class IntroductionComponent {
+  checkAnimationDone: boolean = false;
+
+  constructor(public dataService: DataService) {
+    this.playAnimation();
+  }
+
+  playAnimation() {
+    this.checkAnimationDone = this.dataService.checkIfAnimationIsDone;
+    setTimeout(() => {
+      if (this.checkAnimationDone) {
+        return;
+      }
+      this.checkAnimationDone = this.dataService.afterAnimationDone();
+    }, 3000);
+  }
+}
