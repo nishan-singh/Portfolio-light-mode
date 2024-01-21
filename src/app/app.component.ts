ac518fe6dc5d8c5ab46f5e7aea066b3496a1c31b
Nishan Singh <singh007nishan@gmail.com>
2024-01-21 18:45:52 +0100

    remove unused variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'portfolio-2';
-}
+export class AppComponent {}
